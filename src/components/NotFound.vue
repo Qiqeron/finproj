2154047b9f7ce7cb3539fa7238cd0847028db842
qiqeron <ruslansaakan100@gmail.com>
2023-11-12 23:08:54 +0300

    commit3

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>НЕ НАЙДЕНО</h1>
+        <h2>Пожалуйста, перейдите на главную страницу</h2>
     </div>
 </template>
 
@@ -11,4 +12,14 @@
 </script>
 
 <style scoped>
+    div{
+        margin-top: 10vh;
+        text-align: center;
+    }
+    h1{
+        font-size: 5vw;
+    }
+    h2{
+        font-size: 3vw;
+    }
 </style>
